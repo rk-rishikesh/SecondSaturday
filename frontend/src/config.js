ad031881052cf4de498d382780b1d6f253ee95e8
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2024-12-08 03:22:13 +0530

    Pushed onchainKit Integration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,15 @@
 import { http, createConfig } from 'wagmi'
 import { baseSepolia, polygonAmoy, bscTestnet } from 'wagmi/chains'
 import { metaMask } from 'wagmi/connectors'
+import { coinbaseWallet } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [baseSepolia, polygonAmoy, bscTestnet],
   connectors: [
     metaMask(),
+    coinbaseWallet({
+      appName: 'onchainkit',
+    }),
   ],
   transports: {
     [baseSepolia.id]: http(),
