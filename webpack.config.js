b69ed65ac714a56943406871183433267ee93cf9
Harsh Gupta <harsh.gupta2211@gmail.com>
2024-12-07 12:05:03 +0530

    update data and openai

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// webpack.config.js
+const webpack = require('webpack');
+
+module.exports = {
+    // ... existing configuration ...
+    resolve: {
+        fallback: {
+            "path": require.resolve("path-browserify"),
+            "os": require.resolve("os-browserify/browser"),
+            "crypto": require.resolve("crypto-browserify"),
+            "process": require.resolve("process/browser"),
+            "buffer": require.resolve("buffer/")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    ],
+};
